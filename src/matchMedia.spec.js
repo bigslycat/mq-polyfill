7d70348b9b04f134d4c686a548b6fd46c506bcb4
Pavel Tereschenko <me@pavlik.pro>
2017-02-16 22:02:02 +0300

    Add matchMedia() tests

---
+++ src/matchMedia.spec.js
@@ -0,0 +1,61 @@
+// @flow
+
+/* eslint-env jest */
+
+import createMatchMedia from './matchMedia';
+import MediaQueryList from './MediaQueryList';
+
+describe('matchMedia()', () =>
+  it('Eval MQ and return MediaQueryList object', () => {
+    const matches = 'RESULT';
+    const media = 'MEDIA';
+
+    const evalQuery = jest.fn()
+      .mockReturnValueOnce(matches);
+
+    const addListener = () => {};
+    const removeListener = () => {};
+
+    const matchMedia = createMatchMedia({
+      evalQuery,
+      addListener,
+      removeListener,
+    });
+
+    const mq = matchMedia(media);
+
+    expect(mq).toBeInstanceOf(MediaQueryList);
+
+    expect(Object.getOwnPropertyDescriptor(mq, 'media'))
+      .toEqual({
+        enumerable: true,
+        configurable: false,
+        writable: false,
+        value: media,
+      });
+
+    expect(Object.getOwnPropertyDescriptor(mq, 'matches'))
+      .toEqual({
+        enumerable: true,
+        configurable: true,
+        writable: false,
+        value: matches,
+      });
+
+    expect(Object.getOwnPropertyDescriptor(mq, 'addListener'))
+      .toEqual({
+        enumerable: false,
+        configurable: false,
+        writable: false,
+        value: addListener,
+      });
+
+    expect(Object.getOwnPropertyDescriptor(mq, 'removeListener'))
+      .toEqual({
+        enumerable: false,
+        configurable: false,
+        writable: false,
+        value: removeListener,
+      });
+  }),
+);
